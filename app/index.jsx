745f7bf22fbf2b3c2ac2aa7fa0cc1fdf6b75f4e2
Elijah Afolabi <83648700+ejaymedia@users.noreply.github.com>
2025-09-25 19:08:52 +0100

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -14,5 +14,5 @@ export default Home
 
 const styles = StyleSheet.create({
   container: {flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white"},
-  title: {fontSize: 32, lineHeight: 40, fontWeight: "bold", paddingBottom: 20}
+  title: {fontSize: 32, lineHeight: 40, fontWeight: "bold", paddingBottom: 20},
 })
